ed9b0bbcf04a49e12427b6c56891bafdb13dd7ef
mdmehedyhassan <mdmehedyh55@gmail.com>
2021-06-17 01:17:12 +0600

    main data

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Main from '../Main/Main';
 import TopHeader from '../TopHeader/TopHeader';
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
             <TopHeader></TopHeader>
             <Header></Header>
             <h2 className="p-5 bg-success text-light" style={{textAlign: 'center'}}>The 6 Good reasons to contact us</h2>
+            <Main></Main>
         </div>
     );
 };
--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -11,38 +11,45 @@ const Main = () => {
         {
             id: '1',
             number: 2,
-            title: '',
-            description: ''
+            title: 'Technology: a must have',
+            description: 'Datatellers works with companies that are aware of the importance of investing in innovative technologies to increase competitiveness.'
         },
         {
             id: '2',
             number: 3,
-            title: '',
-            description: ''
+            title: 'Successful results',
+            description: 'Datatellers helps its customers to reduce costs, increase productivity, create new revenue streams from data already present within the organization, stay two steps ahead of competitors.'
         },
         {
             id: '3',
             number: 4,
-            title: '',
-            description: ''
+            title: 'Transform the business, not just the IT department',
+            description: 'Datatellers works with those companies that want solutions that allow them to achieve their business goals, but at the same time are easy to use and implement. The ideal solution is a rapid return on investment that does not require the customer to intervene on the information systems and technologies already present.'
         },
         {
             id: '4',
             number: 5,
-            title: '',
-            description: ''
+            title: 'Data and AI at the service of companies',
+            description: 'Datatellers accompanies its customers towards measurable objectives: 100% ROI in 12 months; + 30% in productivity; -80% risk of human error. Zero business interruption, no system migration.'
         },
         {
             id: '5',
             number: 6,
-            title: '',
-            description: ''
+            title: 'A trusted partner',
+            description: 'Datatellers is a discreet, reliable and trustworthy technology partner. Our confidentiality standards in data processing are those of Defense contracts, our datacenters are hyperconverged and redundant, our systems are full compliance in terms of security.'
         }
     ]
     return (
-        <div>
-            
-
+        <div className="container">
+            <div className="row">
+                {
+                    reasonsData.map(data => <div className="col-md-4 col-sm-6">
+                        <h1>{data.number}</h1>
+                        <h4>{data.title}</h4>
+                        <p>{data.description}</p>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
